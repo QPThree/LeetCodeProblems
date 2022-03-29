332a9f02bb2a8ec13db29f7c47a83a40f86a2a35
Quentin Young <youngqp@amazon.com>
2022-03-29 05:36:55 -0700

    More efficient answer

--- Problems/TwoSum.java
+++ Problems/TwoSum.java
@@ -1,23 +1,20 @@
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
+    public static int[] twoSum(int[] nums, int target) {
         
  
         int[] result = new int[2];
-        int i = 0;
-        for (int num : nums) {
-             int k = i + 1;
-            for (int x : nums) {
-                if ((x + num == target) && (i != k)) {
+        for (int i = 0; i < nums.length; i++){
+            for (int k = i + 1; k < nums.length; k++){
+                if (nums[k] + nums[i] == target){
                     result[0] = i;
                     result[1] = k;
                     return result;
                 }
-                k++;
             }
-           
-            i++;
         }
         return result;
-    }
+    };
+
+   
     
 }
